851753e471e3b354b51637a3605d86e3d37d9785
pankyoking <ckwlghks12+github2@gmail.com>
2021-11-29 05:06:52 +0900

    수정

--- starbucks/index.js
+++ starbucks/index.js
@@ -13,36 +13,38 @@ setInterval(() => {
 // 슬라이드
 const promo = document.querySelector(".down");
 
+const splide = new Splide("#thumbnail-slider", {
+  type: "loop",
+  start: 0,
+  fixedWidth: 840,
+  fixedHeight: 541,
+  autoplay: false,
+  gap: 10,
+  cover: true,
+  focus: "center",
+  isNavigation: true,
+  interval: 2070,
+  resetProgress: true,
+});
+splide.mount();
+
 promo.addEventListener("click", (e) => {
   e.preventDefault();
   e.stopPropagation();
   const banner = document.querySelector(".banners");
   const btn = document.querySelector("span img[role='button']");
+  const flag = 99;
+  const Autoplay = splide.Components.Autoplay;
 
   if (banner.style.display === "block") {
     banner.style.display = "none";
     btn.src = "img/btn_prom_down.png";
+    Autoplay.pause(flag);
   } else {
     banner.style.display = "block";
     btn.src = "img/btn_prom_up.png";
+    Autoplay.play(flag);
   }
-});
-document.addEventListener("DOMContentLoaded", function () {
-  const splide = new Splide("#thumbnail-slider", {
-    type: "loop",
-    start: 0,
-    fixedWidth: 840,
-    fixedHeight: 541,
-    autoplay: true,
-    gap: 10,
-    cover: true,
-    focus: "center",
-    isNavigation: true,
-    interval: 2070,
-    resetProgress: true,
-  });
-  splide.mount();
-  console.log(splide);
 });
 
 AOS.init();
