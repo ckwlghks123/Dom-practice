6fc627dee862e038e4e9d10ecc69f751a31b1ec1
pankyoking <ckwlghks12+github2@gmail.com>
2021-11-29 11:54:44 +0900

    수정

--- starbucks/index.js
+++ starbucks/index.js
@@ -1,3 +1,10 @@
+/**
+ * @Filename index.js
+ * @Author ckwlghks123
+ * @Description 스타벅스 페이지 Dom 기능 작성
+ */
+
+AOS.init();
 // 공지사항
 const news = document.querySelector(".news");
 setInterval(() => {
@@ -15,7 +22,7 @@ const promo = document.querySelector(".down");
 
 const splide = new Splide("#thumbnail-slider", {
   type: "loop",
-  start: 0,
+  start: 2,
   fixedWidth: 840,
   fixedHeight: 541,
   autoplay: false,
@@ -30,21 +37,38 @@ splide.mount();
 
 promo.addEventListener("click", (e) => {
   e.preventDefault();
-  e.stopPropagation();
   const banner = document.querySelector(".banners");
   const btn = document.querySelector("span img[role='button']");
   const flag = 99;
   const Autoplay = splide.Components.Autoplay;
+  const slide = document.querySelector(".slider");
 
   if (banner.style.display === "block") {
     banner.style.display = "none";
+    banner.style.height = "0px";
+
     btn.src = "img/btn_prom_down.png";
     Autoplay.pause(flag);
   } else {
     banner.style.display = "block";
+    banner.style.height = "658px";
+
     btn.src = "img/btn_prom_up.png";
     Autoplay.play(flag);
   }
 });
 
-AOS.init();
+// 검색창
+const search = document.querySelector(".search_btn");
+const searchInput = document.querySelector("#search");
+const topNav = document.querySelector(".top_nav");
+search.addEventListener("click", (e) => {
+  e.preventDefault();
+  topNav.style.right = "232px";
+  topNav.style.transition = "all 1s";
+  search.style.transition = "all 1s";
+  search.style.width = "184px";
+  searchInput.style.width = "134px";
+  searchInput.style.display = "inline-block";
+  searchInput.style.transition = "all 1s";
+});
